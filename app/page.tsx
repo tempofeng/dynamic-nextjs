50e3116e691dbe1f30064e063802acea99780290
Yenwen Feng <tempofeng@gmail.com>
2023-12-21 19:13:29 +0800

    [feature] add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { DynamicWidget } from "../lib/dynamic";
+import { DynamicWidget } from "../lib/dynamic"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <DynamicWidget />
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                <DynamicWidget />
+            </div>
+        </main>
+    )
 }
