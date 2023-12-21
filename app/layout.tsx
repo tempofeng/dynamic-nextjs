1cbbc2df2688cf84250862a0d28685c2ef69325c
Yenwen Feng <tempofeng@gmail.com>
2023-12-21 19:10:16 +0800

    [feature] set env ID in .env

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <DynamicContextProvider
         settings={{
-          environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
+          environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID!,
           walletConnectors: [
             EthereumWalletConnectors,
             FlowWalletConnectors,
