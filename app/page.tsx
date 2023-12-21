7505ad66e2a2bd47ed2f0d700097d392ab11a84c
Yenwen Feng <tempofeng@gmail.com>
2023-12-21 21:26:45 +0800

    [feature] show account and chain id

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
+"use client"
+
 import { DynamicWidget } from "../lib/dynamic"
+import { useAccount, useNetwork } from "wagmi"
+
+const WalletInfo = () => {
+    const { address, isConnected } = useAccount()
+    const { chain } = useNetwork()
+
+    return (
+        <div className="p-6 max-w-5xl w-full items-center justify-between font-mono text-sm">
+            <p>
+                wagmi connected: {isConnected ? "true" : "false"}
+            </p>
+            <p>wagmi address: {address}</p>
+            <p>wagmi network: {chain?.id}</p>
+        </div>
+    )
+}
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <main className="flex flex-col items-center justify-between p-24">
+            <div className="p-6 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <DynamicWidget />
             </div>
+            <WalletInfo />
         </main>
     )
 }
